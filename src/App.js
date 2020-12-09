0c5901bc003c60536abacbf7bd333fa7f1e046fe
silentnoise86 <hushmandres@gmail.com>
2020-12-09 20:37:45 +0200

    changed placeholder text to: "Search all Monsters"

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
         this.state = {
             monsters: [],
             searchField: '',
-            placeholder: 'Search Monsters'
+            placeholder: 'Search All Monsters'
         }
     }
 
